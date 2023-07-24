f7b5f479bcceb00e2b5d8cea2850aa48a1de7af6
Siam10101 <mehedihasansiam030@gmail.com>
2023-07-24 23:25:39 +0600

    first commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import HomeSectionOne from '../../Component/HomeSectionOne/HomeSectionOne';
+import React from "react";
+import HomeSectionOne from "../../Component/HomeSectionOne/HomeSectionOne";
 
 const Home = () => {
-    return (
-        <div>
-            <HomeSectionOne></HomeSectionOne>
-        </div>
-    );
+  return (
+    <div>
+      <HomeSectionOne></HomeSectionOne>
+    </div>
+  );
 };
 
 export default Home;
